f18ae7398aabf5c0ee6e89ce4e12a4602c395bd6
Corbin Crutchley <git@crutchcorn.dev>
2024-04-02 22:09:47 -0700

    chore: upgrade DTS

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { resolve } from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    dts({
-      entryRoot: resolve(__dirname, "./lib"),
-    }),
-  ],
-  optimizeDeps: {
-    include: [
-      "react",
-      "react/jsx-runtime",
-      "react/jsx-dev-runtime",
-      "react-dom",
-    ],
-  },
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
+  plugins: [react()],
 });
--- examples/vanillajs/vite.config.ts
+++ examples/vanillajs/vite.config.ts
@@ -1,15 +1,4 @@
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { resolve } from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export default defineConfig({
-  plugins: [
-    dts({
-      entryRoot: resolve(__dirname, "./lib"),
-    }),
-  ],
-});
+// https://vitejs.dev/config/
+export default defineConfig({});
