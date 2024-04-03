0140e38b5ee57c2ddd97b463144c08adca7e122d
Corbin Crutchley <git@crutchcorn.dev>
2024-04-03 01:57:05 -0700

    chore: add vanillajs example

--- examples/vanillajs/src/index.ts
+++ examples/vanillajs/src/index.ts
@@ -1,7 +1,13 @@
-import { setElement } from "@codewrapper/core";
+import { getState, setElement } from "@codewrapper/core";
+import { StateEffect } from "@codemirror/state";
+import { basicSetup } from "codemirror";
 
-const el = document.createElement("button");
+const el = document.createElement("div");
 
-// setElement(el);
+const view = setElement(el, getState());
+const transaction = view.state.update({
+  effects: StateEffect.appendConfig.of(basicSetup),
+});
+view.dispatch(transaction);
 
 document.body.append(el);
